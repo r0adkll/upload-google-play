babc1cba505151dd994769c7a5dcc8bb1e845004
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-01-25 20:24:44 +1300

    Reconfigure build for ncc and drop node_modules

--- node_modules/googleapis-common/build/src/apirequest.d.ts
+++
@@ -1,9 +0,0 @@
-import { GaxiosPromise } from 'gaxios';
-import { APIRequestParams, BodyResponseCallback } from './api';
-/**
- * Create and send request to Google API
- * @param parameters Parameters used to form request
- * @param callback   Callback when request finished or error found
- */
-export declare function createAPIRequest<T>(parameters: APIRequestParams): GaxiosPromise<T>;
-export declare function createAPIRequest<T>(parameters: APIRequestParams, callback: BodyResponseCallback<T>): void;
