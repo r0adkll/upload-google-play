4e8635ed9c988fa6262bc5151d82cc5c5b148d31
Pablo A. Martínez <pamartinez@boiradev.com>
2021-08-03 16:11:38 +0200

    Updated whatsnew tests

--- __tests__/whatsnew.test.ts
+++ __tests__/whatsnew.test.ts
@@ -7,7 +7,7 @@ import LocalizedText = androidpublisher_v3.Schema$LocalizedText;
 
 test("read localized whatsnew files", async () => {
     let texts = await readLocalizedReleaseNotes("./__tests__");
-    expect(texts).toHaveLength(3);
+    expect(texts).toHaveLength(6);
     expect(texts).toContainEqual({
         language: "en-US",
         text: "test_changelog_file"
@@ -19,5 +19,17 @@ test("read localized whatsnew files", async () => {
     expect(texts).toContainEqual({
         language: "ja-JP",
         text: "test_changelog_file_japanese"
+    });
+    expect(texts).toContainEqual({
+        language: "ca",
+        text: "test_changelog_file_catalan"
+    });
+    expect(texts).toContainEqual({
+        language: "es-ES",
+        text: "test_changelog_file_spanish"
+    });
+    expect(texts).toContainEqual({
+        language: "et",
+        text: "test_changelog_file_estonian"
     });
 });
