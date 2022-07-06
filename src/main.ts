772a71d435e3d7b59d9f66d476d86d3e79dcae41
Thomas Aunvik <thomas.aunvik@exigo.no>
2022-07-06 12:33:01 +0200

    Add Debug Symbol File/Folder Upload
    Uploads Deobfuscation File type 'nativeCode'.
    
    Inputs allowed in 'debugSymbols', a .zip file, or a folder that contains the symbols.

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,7 @@ async function run() {
         const status = core.getInput('status', { required: false });
         const whatsNewDir = core.getInput('whatsNewDirectory', { required: false });
         const mappingFile = core.getInput('mappingFile', { required: false });
+		const debugSymbols = core.getInput('debugSymbols', { required: false });
         const changesNotSentForReview = core.getInput('changesNotSentForReview', { required: false }) == 'true';
         const existingEditId = core.getInput('existingEditId')
 
@@ -107,6 +108,11 @@ async function run() {
             return
         }
 
+		if (debugSymbols != undefined && debugSymbols.length > 0 && !fs.existsSync(debugSymbols)) {
+			core.setFailed(`Unable to find 'debugSymbols' @ ${debugSymbols}`);
+			return
+		}
+
         const authClient = await auth.getClient();
 
         const result = await uploadToPlayStore({
@@ -118,6 +124,7 @@ async function run() {
             status: status,
             whatsNewDir: whatsNewDir,
             mappingFile: mappingFile,
+			debugSymbols: debugSymbols,
             name: releaseName,
             changesNotSentForReview: changesNotSentForReview,
             existingEditId: existingEditId
