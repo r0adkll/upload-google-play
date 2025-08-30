aa3dcddece3c24dad57e7e183b1eb9f80490cff8
Jeremy Kane <jkane001@gmail.com>
2025-08-29 20:30:07 -0400

    Txt extension (#1)
    * Package updates
    * Enabled optional .txt extension for whatsnew files
    * Applied "Prettier" formatting
    * Added and updated tests

--- src/whatsnew.ts
+++ src/whatsnew.ts
@@ -1,41 +1,44 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 import * as fs from "fs";
 import * as path from "path";
-import {androidpublisher_v3} from "@googleapis/androidpublisher";
+import { androidpublisher_v3 } from "@googleapis/androidpublisher";
 import LocalizedText = androidpublisher_v3.Schema$LocalizedText;
-import { readFile } from 'fs/promises';
+import { readFile } from "fs/promises";
 
-export async function readLocalizedReleaseNotes(whatsNewDir: string | undefined): Promise<LocalizedText[] | undefined> {
-    core.debug(`Executing readLocalizedReleaseNotes`);
-    if (whatsNewDir != undefined && whatsNewDir.length > 0) {
-        const releaseNotes = fs.readdirSync(whatsNewDir)
-            .filter(value => /whatsnew-((.*-.*)|(.*))\b/.test(value));
-        const pattern = /whatsnew-(?<local>(.*-.*)|(.*))/;
+export async function readLocalizedReleaseNotes(
+  whatsNewDir: string | undefined
+): Promise<LocalizedText[] | undefined> {
+  core.debug(`Executing readLocalizedReleaseNotes`);
 
-        const localizedReleaseNotes: LocalizedText[] = [];
+  if (whatsNewDir) {
+    const files = await fs.promises.readdir(whatsNewDir);
+    const pattern = /whatsnew-(?<local>[a-z]{2,3}(?:-[A-Z]{2})?)(\.txt)?$/;
+    const localizedReleaseNotes: LocalizedText[] = [];
 
-        core.debug(`Found files: ${releaseNotes.toString()}`);
-        for (const value of releaseNotes) {
-            const matches = value.match(pattern);
-            if (matches != null && matches.length == 4) {
-                core.debug(`Matches for ${value} = ${matches.toString()}`);
-                const lang = matches[1];
-                const filePath = path.join(whatsNewDir, value);
-                const content = await readFile(filePath, 'utf-8');
+    core.debug(`Scanning for whatsnew files in ${whatsNewDir}`);
 
-                if (content != undefined) {
-                    core.debug(`Found localized 'whatsnew-*-*' for Lang(${lang})`);
-                    localizedReleaseNotes.push(
-                        {
-                            language: lang,
-                            text: content
-                        }
-                    )
-                }
-            }
+    for (const file of files) {
+      const matches = file.match(pattern);
+
+      if (matches && matches.groups && matches.groups.local) {
+        const lang = matches.groups.local;
+        const filePath = path.join(whatsNewDir, file);
+        const content = (await readFile(filePath, "utf-8")).trim();
+
+        if (content) {
+          core.debug(
+            `Found localized 'whatsnew' for Lang(${lang}) in file ${file}`
+          );
+          localizedReleaseNotes.push({
+            language: lang,
+            text: content,
+          });
         }
-
-        return localizedReleaseNotes
+      }
     }
-    return undefined
+
+    return localizedReleaseNotes;
+  }
+
+  return undefined;
 }
