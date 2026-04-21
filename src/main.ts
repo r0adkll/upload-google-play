16a5248393ad83c75980bdb6b8e9d4566f26ea66
r0adkll <veedubusc@gmail.com>
2026-04-21 09:31:55 -0400

    Only check changed files in coverage report + fail if coverage < 80

--- src/main.ts
+++ src/main.ts
@@ -78,6 +78,8 @@ export async function run() {
             core.warning(`Unable to find 'debugSymbols' @ ${debugSymbols}`);
         }
 
+        core.debug(`Testing code coverage, pls delete me!`);
+
         await pTimeout(
             runUpload(
                 packageName,
